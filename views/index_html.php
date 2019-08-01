d4be4143ddeaed5b3eb684e33d70a1d1fa501c8d
Gautier Michelin <gm@ideesculture.com>
2019-08-01 11:49:19 +0200

    default view

---
+++ views/index_html.php
@@ -0,0 +1,4 @@
+<?php
+//$va_statistics <li>= $this->getVar('statistics_listing');
+<h1>Boutons INRAP</h1>
+<p>Retrouvez des boutons d'ajout rapide d'objet et de mouvements sous les collections et les objets de la base, via l'onglet Résumé.</p>
